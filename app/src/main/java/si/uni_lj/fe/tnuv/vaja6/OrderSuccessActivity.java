afcc8e8cc761ec1b8a48d5f528542b51c378b494
timvivoda <tim.vivoda@gmail.com>
2022-06-12 16:48:24 +0200

    final_brez_mapsov_baza_json

--- app/src/main/java/si/uni_lj/fe/tnuv/vaja6/OrderSuccessActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/vaja6/OrderSuccessActivity.java
@@ -1,8 +1,13 @@
 package si.uni_lj.fe.tnuv.vaja6;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import si.uni_lj.fe.tnuv.vaja6.model.RestaurantModel;
 
 public class OrderSuccessActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class OrderSuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_success);
+
+        RestaurantModel restaurantModel = getIntent().getParcelableExtra("RestaurantModel");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(restaurantModel.getName());
+        actionBar.setSubtitle(restaurantModel.getAddress());
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+
+        TextView buttonDone = findViewById(R.id.buttonDone);
+        buttonDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
