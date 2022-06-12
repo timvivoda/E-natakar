5e9f839d779d1be1a8e5e7651c94c2893f2d6074
timvivoda <tim.vivoda@gmail.com>
2022-06-12 16:07:52 +0200

    košarica_update

--- app/src/main/java/si/uni_lj/fe/tnuv/vaja6/PlaceYourOrderActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/vaja6/PlaceYourOrderActivity.java
@@ -1,19 +1,30 @@
 package si.uni_lj.fe.tnuv.vaja6;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import si.uni_lj.fe.tnuv.vaja6.adapters.PlaceYourOrderAdapter;
+import si.uni_lj.fe.tnuv.vaja6.model.Menu;
 import si.uni_lj.fe.tnuv.vaja6.model.RestaurantModel;
 
 public class PlaceYourOrderActivity extends AppCompatActivity {
 
-    private EditText inputName, inputAddress, inputCity, inputState, inputZip,inputCardNumber, inputCardExpiry, inputCardPin ;
+    private EditText inputName, inputAddress, inputCity, inputZip,inputCardNumber, inputCardExpiry, inputCardPin ;
     private RecyclerView cartItemsRecyclerView;
     private TextView tvSubtotalAmount, tvDeliveryChargeAmount, tvDeliveryCharge, tvTotalAmount, buttonPlaceYourOrder;
     private SwitchCompat switchDelivery;
@@ -26,13 +37,134 @@ public class PlaceYourOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_place_your_order);
 
 
-
         RestaurantModel restaurantModel = getIntent().getParcelableExtra("RestaurantModel");
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(restaurantModel.getName());
         actionBar.setSubtitle(restaurantModel.getAddress());
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        inputName = findViewById(R.id.inputName);
+        inputAddress = findViewById(R.id.inputAddress);
+        inputCity = findViewById(R.id.inputCity);
+        inputZip = findViewById(R.id.inputZip);
+        inputCardNumber = findViewById(R.id.inputCardNumber);
+        inputCardExpiry = findViewById(R.id.inputCardExpiry);
+        inputCardPin = findViewById(R.id.inputCardPin);
+        tvSubtotalAmount = findViewById(R.id.tvSubtotalAmount);
+        tvDeliveryChargeAmount = findViewById(R.id.tvDeliveryChargeAmount);
+        tvDeliveryCharge = findViewById(R.id.tvDeliveryCharge);
+        tvTotalAmount = findViewById(R.id.tvTotalAmount);
+        buttonPlaceYourOrder = findViewById(R.id.buttonPlaceYourOrder);
+        switchDelivery = findViewById(R.id.switchDelivery);
 
+        cartItemsRecyclerView = findViewById(R.id.cartItemsRecyclerView);
+
+        buttonPlaceYourOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPlaceOrderButtonClick(restaurantModel);
+
+            }
+        });
+
+        switchDelivery.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    inputAddress.setVisibility(View.VISIBLE);
+                    inputCity.setVisibility(View.VISIBLE);
+                    inputZip.setVisibility(View.VISIBLE);
+                    tvDeliveryChargeAmount.setVisibility(View.VISIBLE);
+                    tvDeliveryCharge.setVisibility(View.VISIBLE);
+                    isDeliveryOn = true;
+                    calculateTotalAmount(restaurantModel);
+                } else {
+                    inputAddress.setVisibility(View.GONE);
+                    inputCity.setVisibility(View.GONE);
+                    inputZip.setVisibility(View.GONE);
+                    tvDeliveryChargeAmount.setVisibility(View.GONE);
+                    tvDeliveryCharge.setVisibility(View.GONE);
+                    isDeliveryOn = false;
+                    calculateTotalAmount(restaurantModel);
+                }
+            }
+        });
+
+        initRecyclerView(restaurantModel);
+        calculateTotalAmount(restaurantModel);
+    }
+
+    private void calculateTotalAmount(RestaurantModel restaurantModel) {
+        float subTotalAmount = 0f;
+
+        for(Menu m : restaurantModel.getMenus()) {
+            subTotalAmount += m.getPrice() * m.getTotalInCart();
+        }
+
+        tvSubtotalAmount.setText("€"+String.format("%.2f", subTotalAmount));
+        if(isDeliveryOn) {
+            tvDeliveryChargeAmount.setText("€"+String.format("%.2f", restaurantModel.getDelivery_charge()));
+            subTotalAmount += restaurantModel.getDelivery_charge();
+        }
+        tvTotalAmount.setText("€"+String.format("%.2f", subTotalAmount));
+    }
+
+        private void onPlaceOrderButtonClick(RestaurantModel restaurantModel) {
+            if(TextUtils.isEmpty(inputName.getText().toString())) {
+                inputName.setError("Vnesite svoje ime ");
+                return;
+            } else if(isDeliveryOn && TextUtils.isEmpty(inputAddress.getText().toString())) {
+                inputAddress.setError("Vnesite svoj naslov ");
+                return;
+            }else if(isDeliveryOn && TextUtils.isEmpty(inputCity.getText().toString())) {
+                inputCity.setError("Vnesite svoj kraj bivanja ");
+                return;
+            }else if( TextUtils.isEmpty(inputCardNumber.getText().toString())) {
+                inputCardNumber.setError("Vnesite številko kartice ");
+                return;
+            }else if( TextUtils.isEmpty(inputCardExpiry.getText().toString())) {
+                inputCardExpiry.setError("Vnesite potek veljavnosti kartice ");
+                return;
+            }else if( TextUtils.isEmpty(inputCardPin.getText().toString())) {
+                inputCardPin.setError("Vnesite CVV kodo ");
+                return;
+            }
+            //start success activity..
+            Intent i = new Intent(PlaceYourOrderActivity.this, OrderSuccessActivity.class);
+            i.putExtra("RestaurantModel", restaurantModel);
+            startActivityForResult(i, 1000);
+        }
+
+    private void initRecyclerView(RestaurantModel restaurantModel) {
+        cartItemsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        placeYourOrderAdapter = new PlaceYourOrderAdapter(restaurantModel.getMenus());
+        cartItemsRecyclerView.setAdapter(placeYourOrderAdapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 1000) {
+            setResult(Activity.RESULT_OK);
+            finish();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home :
+                finish();
+            default:
+                //do nothing
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        setResult(Activity.RESULT_CANCELED);
+        finish();
     }
 }
