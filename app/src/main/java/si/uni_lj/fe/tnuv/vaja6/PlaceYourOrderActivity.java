fc4f416068366681f0b39e7f1ba862bf391164c8
timvivoda <tim.vivoda@gmail.com>
2022-06-12 12:00:28 +0200

    design_checkout

--- app/src/main/java/si/uni_lj/fe/tnuv/vaja6/PlaceYourOrderActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/vaja6/PlaceYourOrderActivity.java
@@ -1,14 +1,38 @@
 package si.uni_lj.fe.tnuv.vaja6;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import si.uni_lj.fe.tnuv.vaja6.model.RestaurantModel;
 
 public class PlaceYourOrderActivity extends AppCompatActivity {
+
+    private EditText inputName, inputAddress, inputCity, inputState, inputZip,inputCardNumber, inputCardExpiry, inputCardPin ;
+    private RecyclerView cartItemsRecyclerView;
+    private TextView tvSubtotalAmount, tvDeliveryChargeAmount, tvDeliveryCharge, tvTotalAmount, buttonPlaceYourOrder;
+    private SwitchCompat switchDelivery;
+    private boolean isDeliveryOn;
+    private PlaceYourOrderAdapter placeYourOrderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_your_order);
+
+
+
+        RestaurantModel restaurantModel = getIntent().getParcelableExtra("RestaurantModel");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(restaurantModel.getName());
+        actionBar.setSubtitle(restaurantModel.getAddress());
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
